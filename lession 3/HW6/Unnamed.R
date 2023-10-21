bd7aac72bbad1814dd7318616c0f98a5349047ed
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-10-21 05:26:13 +0300

    HW6

--- lession 3/HW6/Unnamed.R
+++ lession 3/HW6/Unnamed.R
@@ -33,6 +33,7 @@ plot1 <- function (df) {
 mask <- df_updated$Age < 100
 df_updated <- df[mask, ]
 print(plot1(df_updated))
+Sys.sleep(10)
 
 plot2 <- function(df) {
   fig <- plot_ly(df, 
@@ -41,15 +42,16 @@ plot2 <- function(df) {
     z = ~Income_avg_edu,
     type = "heatmap",
     colors = colorRamp(c("green", "yellow", "brown", "red")))
-  fig <- fig %>% layout(title = 'Тепловая карта по температуре дна при вылове в
-        разрезе генедра и года', xaxis = list(showgrid = FALSE, title='Гендер'),
-                        yaxis = list(showgrid = FALSE, title='Год вылова'))
+  fig <- fig %>% layout(title = 'Тепловая карта по среднему доходу от образованию и Marital_Status',
+                        xaxis = list(showgrid = FALSE, title='Education'),
+                        yaxis = list(showgrid = FALSE, title='Marital_Status'))
   return(fig)
 }
 options(dplyr.summarise.inform = FALSE)
 df2 <- df %>% filter(!is.na(Income)) %>% group_by(Education, Marital_Status) %>% dplyr::summarise(Income_avg_edu = mean(Income))
 df2 <- merge(x=df, y=df2, by.x=c("Education", "Marital_Status"), by.y=c("Education", "Marital_Status"), suffix = c("", ".aggr"))
 print(plot2(df2))
+Sys.sleep(10)
 
 
 plot3 <- function(labels, parents) {
@@ -65,18 +67,20 @@ plot3 <- function(labels, parents) {
   return(fig)
 }
 print(plot3(labels=c("Сотрудник 1", "Сотрудник 2", "Реклама 1", "Реклама 2", "Реклама 3"), parents=c("", "", "Сотрудник 1", "Сотрудник 1", "Сотрудник 2")))
-
+Sys.sleep(10)
 
 plot4 <- function(longitude, latitude) {
-  m <- leaflet() %>%
-    addTiles("Any title:)") %>%  # Add default OpenStreetMap map tiles
-    addMarkers(lng=longitude , lat=latitude, popup="The birthplace of R")
+  m <- leaflet()
+  m <- addTiles(m)
+  m <- addMarkers(m, lng=longitude , lat=latitude, popup="point")
   return(m)
 }
-
-print(plot4(longitude=37.618423, latitude=55.751244))
+#if i pass  %>% it doesn't plot? and i don't know why. btw point is out university. 
+print(plot4(longitude=37.609074, latitude=55.787918))
+Sys.sleep(10)
 
 print(datatable(df))
+Sys.sleep(10)
 plot6 <- function(df) {
   rpivotTable(df, rows="Education", cols="Marital_Status",width="100%", height="400px")
 }
