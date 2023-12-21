78e97719418f0fc06f016dec4406ecc62e5dd3de
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-12-21 06:38:13 +0300

    final_task_for_university
    fyck R, this shitty, unfinished, language of not programming.

--- Unnamed.R
+++ Unnamed.R
@@ -8,15 +8,120 @@
 #install.packages("readr")
 #install.packages("lubridate")
 #install.packages("shinydashboard")
+#install.packages("qdap")
+# install.packages("gtools")
 library(dplyr)
-library(plotly)
-library(tidyr)
+library(qdap)
+library(gtools)
+# library(sqldf)
+#library(plotly)
+#library(tidyr)
 #library(leaflet)
 #library(DT)
 #library(rpivotTable)
-library(shiny)
-setwd("C:/Users/koly36/Desktop/RScripts")
+setwd("C:/Users/vniiz/Desktop/del_it/R_Lemanskiy_Konstantin")
+
+
+set.seed(100010001)
+# я вроде как 16 и человек у нас вроде 25 (я спецально взял только 100 значений что бы не было 100% совподения распределения и выборки)
+k <- 16
+n <- 25
+data_length = 100
+norm_variaty <- rnorm(n=data_length, mean=k*100, sd=sqrt(n + k))
+binom_variaty <- rbinom(n=data_length, size=n, prob=k/n)
+chi_sqr_variaty <- rchisq(n=data_length, df=(n - 1 + k))
+
+
+my_describe <- function(data) {
+  cat("mean:", mean(data), "\n")
+  cat("dispersion:", sd(data) ** 2, "\n")
+  cat("mode:", mode(data), "\n")
+  cat("median:", median(data), "\n")
+}
+cat("1. norm_variaty", "\n", sep="")
+my_describe(norm_variaty)
+cat("\n", "2. binom_variaty", "\n", sep="")
+my_describe(binom_variaty)
+cat("\n", "3. chi_sqr_variaty", "\n", sep="")
+my_describe(chi_sqr_variaty)
+
+options(repr.plot.width=6, repr.plot.height=10)
+x <- seq(min(norm_variaty), max(norm_variaty), length = 100)
+y <- dnorm(x, mean=k*100, sd=sqrt(n + k))
+plot(density(norm_variaty))
+par(new=TRUE)
+plot(x, y, xaxt='n', yaxt='n')
 
 
 
-runApp(display.mode="showcase")
+x <- seq(min(binom_variaty), max(binom_variaty), length = max(binom_variaty) - min(binom_variaty) + 1)
+y <- dbinom(x, size=n, prob=k/n)
+plot(density(binom_variaty)) 
+par(new=TRUE)
+plot(x, y, xaxt='n', yaxt='n')
+# plot(density(chi_sqr_variaty)) 
+
+# options(repr.plot.width=6, repr.plot.height=100)
+x <- seq(min(chi_sqr_variaty), max(chi_sqr_variaty), length = 100)
+y <- dchisq(x, df=(n - 1 + k))
+plot(density(chi_sqr_variaty))
+par(new=TRUE)
+plot(x, y, xaxt='n', yaxt='n')
+
+
+cat("задание 2:", "\n")
+cat("1)", "\n")
+cat("a)", (choose(35, 2) + choose(25, 3)) / choose(60, 5), "\n")
+# вероятность того что ремантируется хотя бы один автомобиль ф.р. = 1- вероятности что не ремантируется ни одного авт. ф.р.
+cat("б)", 1 - ((choose(25, 5)) / choose(60, 5)), "\n")
+cat("2)", "\n")
+# ф-ла включений исключений
+s <- 0
+for (i in 1:360) {
+  s <- s + (-1) ** (i + 1) * choose(360, i) * ((1/365) ** i)
+}
+cat("a)", s, "\n")
+magicFun <- function(n_p){
+  s <- 0
+  for (i in 1:n_p) {
+    s <- s + (-1) ** (i + 1) * choose(n_p, i) * ((1/365) ** i)
+  }
+  return(s > 0.5)
+}
+# на самом деле binsearch тут не обьязательно я просто сначала накосячил и сделал бесконечный цикл, так что пусть будет.
+x <- binsearch(magicFun, range = c(1,360), showiter = F, target = 0.5)
+cat("б)", x$where[1], "\n")
+
+
+
+# Не играл в покер. Только где то видел какую то разновидность в какой то компьютерной игрушке, так что за комбинации шарю.
+cat("задание 3:", "\n")
+cards_count <- 52
+# 52 / 4 = 13
+# всего комбинаций
+all_comb <- choose(52, 5)
+# на самом деле не так уж и много. Можно и перебрать.
+p_royal_flesh <- 4 / all_comb
+cat("ь)", p_royal_flesh, "\n")
+p_street_flesh <- 8 * 4 / all_comb
+cat("з)", p_street_flesh, "\n")
+p_care <- 13 * 48 / all_comb
+cat("ж)", p_care, "\n")
+p_tree_two <- 13 * choose(4, 3) * 12 * choose(4, 2) / all_comb
+cat("е)", p_tree_two, "\n")
+p_flesh <- 4 * choose(13, 5) / all_comb  - p_street_flesh - p_royal_flesh
+cat("д)", p_flesh, "\n")
+# если здесь заменить 13 в двух местах на 10 то ответы сойдутся с https://www.nkj.ru/archive/articles/2463/#:~:text=%D0%9E%D0%B1%D1%89%D0%B5%D0%B5%20%D1%87%D0%B8%D1%81%D0%BB%D0%BE%20%D0%B2%D1%81%D0%B5%D1%85%20%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D1%8B%D1%85%20%D0%BA%D0%BE%D0%BC%D0%B1%D0%B8%D0%BD%D0%B0%D1%86%D0%B8%D0%B9,21%2C%20%D0%BF%D0%B0%D1%80%D1%83%201%2F2.
+# так 
+p_street <- 13 * (4 ** 5) / all_comb - p_street_flesh - p_royal_flesh - 13 * 4 / all_comb
+cat("г)", p_street, "\n")
+p_tree <- 13 * choose(4, 3) *  choose(48, 2) / all_comb - p_tree_two
+cat("в)", p_tree, "\n")
+p_two_two <- choose(13, 2) * (choose(4, 2) ** 2) * 44 / all_comb
+cat("б)", p_two_two, "\n")
+# p_two <- 13 * choose(4, 2) * choose(48, 3) / all_comb - 12 * choose(4, 3) / all_comb -  12 * choose(4, 2)* (11 * 4) / all_comb
+# # я учел два-два и три-два а есть еще флеш
+# # p_two <- p_two - 
+# cat("а)", p_two, "\n")
+# в двойке много включений исключений.... крч душно. Так что на двойку я забил.
+
---
+++ final_task2.R
@@ -0,0 +1,178 @@
+#install.packages("shiny")
+#install.packages("shinydashboard")
+#install.packages("readr")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("plotly")
+#install.packages("sqldf")
+# install. packages('xlsx') 
+# install.packages("mltools")
+# install.packages("DescTools")
+# install.packages("ggpubr")
+# install.packages("psych")
+# install.packages("rcompanion")
+# install.packages("caTools")
+# library(shiny)
+# library(shinydashboard)
+# library (xlsx)
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(plotly)
+library(sqldf)
+library(mltools)
+library(DescTools)
+library(data.table)
+library(ggpubr)
+library (psych)
+library (rcompanion)
+library(caTools)
+library(Metrics) 
+setwd("C:/Users/vniiz/Desktop/del_it/R_Lemanskiy_Konstantin")
+set.seed(1)
+# campaign_desc <- read_csv("data/campaign_desc.csv", col_types = "ciii")
+# campaign_table <- read_csv("data/campaign_table.csv", col_types = "cii")
+# causal_data <- read_csv("data/causal_data.csv", col_types = "iiicc")
+# coupon <- read_csv("data/coupon.csv", col_types = "cii")
+# coupon_redempt <- read_csv("data/coupon_redempt.csv", col_types = "iici")
+hh_demographic <- read_csv("data/hh_demographic.csv", col_types = "ccccccc")
+#product <- read_csv("data/product.csv", col_types = "iiccccc")
+transaction_data <- read_csv("data/transaction_data.csv", col_types = "iciiididiidd")
+overall_df <- sqldf("select
+            t1.household_key,
+            t1.BASKET_ID,
+            t1.DAY,
+            t1.PRODUCT_ID,
+            t1.QUANTITY,
+            t1.SALES_VALUE,
+            t1.STORE_ID,
+            t1.RETAIL_DISC,
+            t1.TRANS_TIME,
+            t1.WEEK_NO,
+            t1.COUPON_DISC,
+            t1.COUPON_MATCH_DISC,
+            t2.AGE_DESC,
+            t2.MARITAL_STATUS_CODE,
+            t2.INCOME_DESC,
+            t2.HOMEOWNER_DESC,
+            t2.HH_COMP_DESC,
+            t2.HOUSEHOLD_SIZE_DESC,
+            t2.KID_CATEGORY_DESC
+      FROM transaction_data as t1
+      INNER JOIN hh_demographic as t2
+      ON t1.household_key = t2.household_key")
+
+
+avg_daily_check <- sqldf("
+    SELECT 
+      t1.household_key,
+      sum(t1.daily_check) / count(t1.daily_check) as avg_day_daily_check
+    FROM 
+      (SELECT
+            household_key,
+            DAY,
+            sum(SALES_VALUE) as daily_check
+      FROM transaction_data
+      GROUP BY household_key, DAY) as t1
+    GROUP BY t1.household_key")
+
+avg_daily_check_by_hh <- sqldf("
+    SELECT 
+      t1.household_key,
+      sum(t1.avg_day_daily_check) / count(t1.avg_day_daily_check) as avg_hh_daily_check
+    FROM avg_daily_check as t1
+    GROUP BY t1.household_key")
+
+df <- sqldf("
+    SELECT 
+      t2.*,
+      t1.avg_hh_daily_check as target
+    FROM avg_daily_check_by_hh as t1
+    INNER JOIN hh_demographic as t2
+    ON t1.household_key = t2.household_key")
+
+barplot(table(df$AGE_DESC), main = "AGE_DESC")
+barplot(table(df$MARITAL_STATUS_CODE), main = "MARITAL_STATUS_CODE")
+barplot(table(df$INCOME_DESC), main = "INCOME_DESC")
+barplot(table(df$HOMEOWNER_DESC), main = "HOMEOWNER_DESC")
+barplot(table(df$HH_COMP_DESC), main = "HH_COMP_DESC")
+barplot(table(df$KID_CATEGORY_DESC), main = "KID_CATEGORY_DESC")
+barplot(table(df$HOUSEHOLD_SIZE_DESC), main = "HOUSEHOLD_SIZE_DESC")
+boxplot(df$target, main = "average daily check by hh")
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+cat("mean:", mean(df$target), "median:", median(df$target), "\nmoda:", toString(getmode(df$target)), "std:", sd(df$target), "\n")
+
+# cat(unique(df$HOMEOWNER_DESC))
+
+# corr_matrix <- cramerV(subset(df, select = -c(target)))
+# View(corr_matrix)
+
+# print(df)
+# cat(sapply(df, class))
+df$AGE_DESC <- as.factor(df$AGE_DESC)
+df$MARITAL_STATUS_CODE <- as.factor(df$MARITAL_STATUS_CODE)
+df$INCOME_DESC <- as.factor(df$INCOME_DESC)
+# df$HOMEOWNER_DESC <- as.factor(df$HOMEOWNER_DESC)
+df$HOMEOWNER_DESC <- as.factor(df$HOMEOWNER_DESC)
+df$HH_COMP_DESC <- as.factor(df$HH_COMP_DESC)
+df$KID_CATEGORY_DESC <- as.factor(df$KID_CATEGORY_DESC)
+df <- one_hot(as.data.table(df), cols = "auto", dropCols = TRUE)
+# print(df)
+# cat(sapply(df, class))
+# cat(unique(df$HOUSEHOLD_SIZE_DESC))
+f <- function(x) {
+  if (x == "5+") {
+    return ("5")
+  }
+  return(x)
+}
+df$HOUSEHOLD_SIZE_DESC <- sapply(df$HOUSEHOLD_SIZE_DESC, f)
+df$HOUSEHOLD_SIZE_DESC <- as.integer(df$HOUSEHOLD_SIZE_DESC)
+cat("data types:", sapply(df, class))
+rownames(df) <- df$household_key
+df$household_key <- NULL
+# print(df)
+
+plot(density(df$target), main="kde target") 
+hist(df$target, main="hist target") 
+plot(df$HOUSEHOLD_SIZE_DESC, df$target, xlab="HOUSEHOLD_SIZE_DESC", ylab="target")
+print(ggqqplot(df$target))
+
+corr_matrix <- tetrachoric(subset(df, select = -c(target, HOUSEHOLD_SIZE_DESC)))
+View(corr_matrix$rho)
+# View(subset(df, select = -c(target, HOUSEHOLD_SIZE_DESC)))
+# x <- rownames(corr_matrix)
+# y <- colnames(corr_matrix)
+# data <- expand.grid(X=x, Y=y)
+# data$Z <- corr_matrix$rho
+# 
+# # Heatmap
+# ggplot(df, aes(X, Y, fill= Z)) +
+#   geom_tile()
+# write.csv(corr_matrix$rho, "final_task/data.csv") 
+
+# регресия на двух признаках:
+model <- lm(target ~ HOUSEHOLD_SIZE_DESC, data=df)
+plot(target ~ HOUSEHOLD_SIZE_DESC, data=df, pch = 16, col = "blue") #Plot the results
+print(abline(model))
+cat("shitty model mae:", mae(df$target,predict(model, newdata=subset(df, select = c(HOUSEHOLD_SIZE_DESC)))), "\n")
+
+
+
+
+sample <- sample.split(df$target, SplitRatio = 0.7)
+train  <- subset(df, sample == TRUE)
+train_label <- train$target
+train_features <- subset(train, select = -c(target))
+test   <- subset(df, sample == FALSE)
+test_label <- test$target
+test_features <- subset(test, select = -c(target))
+
+model <- lm(target ~ ., data=train)
+print(summary(model))
+cat("train mae result:", mae(train_label,predict(model, newdata=train_features)), "\n")
+cat("test mae result:", mae(test_label,predict(model, newdata=test_features)))
